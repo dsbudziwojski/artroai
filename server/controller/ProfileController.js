fcc6592727196deef0606239f9b7ff6132018053
David Budziwojski <dsbudziwojski@gmail.com>
2025-05-06 23:48:12 -0700

    BACKEND: Profile POST endpoint (createProfile)

---
+++ server/controller/ProfileController.js
@@ -0,0 +1,25 @@
+import {PrismaClient} from "../generated/prisma/index.js";
+const prisma = new PrismaClient();
+
+export const createProfile = async (req, res) => {
+    const { username, first_name, last_name, bio, email} = req.body
+    if (username == "" || first_name == "" ||last_name == "" || email == "" || email == "") {
+        throw new Error("required input was not valid")
+    }
+    try {
+        await prisma.user_profile.create({
+            data: {
+                username: username,
+                first_name: first_name,
+                last_name: last_name,
+                bio: bio,
+                email: email,
+                isadmin: false
+            },
+        })
+        res.status(200).json({username: username})
+    } catch (error) {
+        res.status(400).json({errorMsg: error.message})
+    }
+}
+
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+import express from 'express'
+import {createProfile} from "./controller/ProfileController.js";
 const app = express()
 const port = process.env.PORT || 5001
 
+app.use(express.json())
 app.get('/api/test', (req, res) => {
   res.json({serverMsg: 'Hello from server!'})
 })
+
+app.post('/api/users', createProfile)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
