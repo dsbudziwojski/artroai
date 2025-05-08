390b5ac7e69d01fb7c81bb05ada8fb732ca90638
Oliver Cai <olivercai1234@gmail.com>
2025-05-08 11:26:51 -0700

    Created Login page, Profile page, Home page, and Not Found page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
     const [serverMsg, setServerMsg] = useState("I got nothing!")
-
+    const navigate = useNavigate();
     useEffect(() => {
         fetch('/api/test')
             .then(resp => resp.json())
@@ -12,9 +13,8 @@ function Home() {
 
     return (
         <>
-            <p>
-                Message: {serverMsg}
-            </p>
+            <h1>Message: {serverMsg}</h1>
+            <button onClick={() => navigate("/profile")}>User Profile</button>
         </>
     );
 }
