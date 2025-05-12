090dced230d1f452e7ba65c88f13054c9570b5fe
David Budziwojski <dsbudziwojski@gmail.com>
2025-05-11 19:36:49 -0700

    BACKEND: updated schema to manage image locations better, add GET unique image endpoint, templated POST image endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express'
-import { createProfile, getProfile } from "./controller/ProfileController.js";
+import path from 'path'
+import { fileURLToPath } from 'url'
+import {createProfile, getProfile, getUniqueImage} from "./controller/ProfileController.js";
+
 const app = express()
 const port = process.env.PORT || 5001
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(express.json())
+app.use('/api/generated-images', express.static(path.join(__dirname, 'generated-images')));
+
 app.get('/api/test', (req, res) => {
   res.json({serverMsg: 'Hello from server!'})
 })
@@ -11,6 +18,8 @@ app.get('/api/test', (req, res) => {
 app.post('/api/users', createProfile)
 
 app.get('/api/users/:username', getProfile)
+
+app.get('/api/users/:username/images/:image_id', getUniqueImage)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
