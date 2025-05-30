2afe98e6ffceb381793587fdba552c59586177c4
hyojeong, choi <hchoi32@pasadena.edu>
2025-05-30 01:21:49 -0700

    add icon

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import {NavLink, useNavigate} from 'react-router-dom';
+import logo from '../artroai.png';
 
 function Login() {
     const [username, setUsername] = useState("");
@@ -8,6 +9,7 @@ function Login() {
     return (
         <div className="bg-zinc-900 h-screen flex justify-center items-center">
             <div className="bg-zinc-800 p-10 rounded-lg text-center flex flex-col gap-2 w-96">
+                <img src={logo} alt="ArtroAI Logo" className="w-22 h-22 mx-auto mb-4" />
                 <h1 className="text-2xl text-white mb-2">Login</h1>
                 <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="text" placeholder="Username" onChange={(e) => {setUsername(e.target.value)}} required/>
                 <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} required/>
