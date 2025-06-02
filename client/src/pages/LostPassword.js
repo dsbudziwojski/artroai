396b9c7b6b916a32f99d1c87b0de8cb792a8820a
David Budziwojski <dsbudziwojski@gmail.com>
2025-06-01 17:26:55 -0700

    FRONTEND: added tailwind to lost password page

--- client/src/pages/LostPassword.js
+++ client/src/pages/LostPassword.js
@@ -1,12 +1,16 @@
-import { useNavigate } from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
+import logo from "../artroai.png";
 
 function LostPassword() {
     const navigate = useNavigate();
     return (
-        <>
-            <h1>That's a you problem buddy</h1>
-            <button onClick={() => navigate("/")}>Go back to Login</button>
-        </>
+        <div className="bg-zinc-900 h-screen flex justify-center items-center">
+            <div className="bg-zinc-800 p-10 rounded-lg text-center flex flex-col gap-2 w-96">
+                <h1 className="text-2xl text-white mb-2">That's a you problem buddy</h1>
+                <NavLink to={"/"} className="text-violet-400 text-sm mt-4"><button onClick={() => navigate("/")}>Go back to Login →</button></NavLink>
+            </div>
+        </div>
+
     );
 }
 
