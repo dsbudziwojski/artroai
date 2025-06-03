db29c7fc3f68eadaa3e2072cf34d6c2c4d4567f7
David Budziwojski <dsbudziwojski@gmail.com>
2025-06-02 21:09:10 -0700

    FRONTEND: fixed handleSignup authentication issue

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -2,10 +2,11 @@ import {useState} from "react";
 import { NavLink, useNavigate } from 'react-router-dom';
 import logo from '../artroai.png';
 import {auth} from '../firebase';
-import { createUserWithEmailAndPassword, updateProfile }    from "firebase/auth";   
+import {createUserWithEmailAndPassword, getIdToken, updateProfile} from "firebase/auth";
 
 function SignUp() {
-    const [name, setName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -15,34 +16,39 @@ function SignUp() {
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const firebaseUser = userCredential.user;
-
             await updateProfile(firebaseUser, {
-                displayName: name,
+                displayName: firstName,
             });
+            if (!auth.currentUser) {
+                return;
+            }
+            const idToken =  await getIdToken(auth.currentUser, false)
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${idToken}`
+            }
+            const username = firebaseUser.email.split("@")[0]
 
             const response = await fetch("/api/users", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-
-                },
-
+                headers: headers,
                 body: JSON.stringify({
-                    username: "",
-                    first_name: "",
-                    last_name: "",
+                    username: username,
+                    first_name: firstName,
+                    last_name: lastName,
                     email: firebaseUser.email,
                     bio: "",
                     isadmin:false
                 }),
             });
 
-            if (!response.ok) throw new Error("Failed create user profile");
+            if (!response.ok) {
+                throw new Error("Failed create user profile");
+            }
             navigate("/home");
-
         } catch (error){
-            console.error("Sign-up error:", error.message);
-            alert("Sign-up failed: "+ error.message);
+            console.error("Sign-up error:" , error.message);
+            alert("Sign-up failed: " + error.message);
         }
     };
     
@@ -51,7 +57,8 @@ function SignUp() {
             <div className="bg-zinc-800 p-10 rounded-lg text-center flex flex-col gap-2 w-96">
                 <img src={logo} className="rounded-lg w-50 h-auto mx-auto mb-4" />
                 <h1 className="text-2xl text-white mb-2">Sign Up</h1>
-                <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="text" placeholder="Name" onChange={(e) => {setName(e.target.value)}} required/>
+                <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="text" placeholder="First Name" onChange={(e) => {setFirstName(e.target.value)}} required/>
+                <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="text" placeholder="Last Name" onChange={(e) => {setLastName(e.target.value)}} required/>
                 <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} required/>
                 <input className="p-1 bg-zinc-700 text-sm text-zinc-300 placeholder-zinc-400 rounded" type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} required/>
                 <button className="bg-violet-500 text-sm text-zinc-100 p-1.5 w-full rounded" onClick={handleSignUp}>Sign Up</button>
